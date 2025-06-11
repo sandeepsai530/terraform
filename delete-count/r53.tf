a032d8078cdcf1c623ce44dbc341bfc482464157
sandeepsai530 <sandeepsai530@gmail.com>
2025-06-11 15:55:08 +0530

    added ec2

--- delete-count/r53.tf
+++ delete-count/r53.tf
@@ -6,3 +6,11 @@ resource "aws_route53_record" "expense" {
   ttl     = 1
   records = [aws_instance.expense[count.index].private_ip]
 }
+
+resource "aws_route53_record" "expense" {
+  zone_id = var.zone_id
+  name    = "${var.instance[2]}.${var.domain_name}"  #interpolation
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.expense[2].public_ip]
+}
