bbba2b21a41bb5961e7fffaa7134decf43f6e42b
sandeepsai530 <sandeepsai530@gmail.com>
2025-06-11 16:12:58 +0530

    added ec2

--- delete-count/r53.tf
+++ delete-count/r53.tf
@@ -5,6 +5,7 @@ resource "aws_route53_record" "expense" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.expense[count.index].private_ip]
+  allow_overwrite = true
 }
 
 resource "aws_route53_record" "frontend" {
@@ -13,4 +14,5 @@ resource "aws_route53_record" "frontend" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.expense[2].public_ip]
+  allow_overwrite = true
 }
