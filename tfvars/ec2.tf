97e369c204d896e2e75bce740c21fd8c056f0fb8
sandeepsai530 <sandeepsai530@gmail.com>
2025-06-12 20:32:54 +0530

    added ec2

--- tfvars/ec2.tf
+++ tfvars/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "expense  " {
+resource "aws_instance" "expense" {
   count = length(var.instances)
   ami                    = "ami-09c813fb71547fc4f"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
