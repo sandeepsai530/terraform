30df03101424bb20716e4336c84935043fffff44
sandeepsai530 <sandeepsai530@gmail.com>
2025-06-14 09:02:04 +0530

    added ec2

--- ec2-module-test/provider.tf
+++ ec2-module-test/provider.tf
@@ -1,16 +1,16 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "5.84.0"
+      source  = "hashicorp/aws"
+      version = "5.84.0"
     }
   }
 
-    backend "s3" {
-        bucket = "saisandeep-devops-prod"
-        key    = "module-demo" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
-        region = "us-east-1"
-        dynamodb_table = "saisandeep-devops-prod"
+  backend "s3" {
+    bucket         = "saisandeep-devops-prod"
+    key            = "module-demo" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
+    region         = "us-east-1"
+    dynamodb_table = "saisandeep-devops-prod"
   }
 }
 
