88190ad53701029c1156af2f0dd26b87c1110709
sandeepsai530 <sandeepsai530@gmail.com>
2025-06-16 19:33:24 +0530

    added ec2

--- terraform-aws-vpc/peering.tf
+++ terraform-aws-vpc/peering.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc_peering_connection" "foo" {
+resource "aws_vpc_peering_connection" "default" {
     count = var.is_peering_required ? 1 : 0 
 
     peer_vpc_id = local.default_vpc_id #acceptor
@@ -11,4 +11,32 @@ resource "aws_vpc_peering_connection" "foo" {
             Name = "${local.resource_name}-default"
         }
     )
+}
+
+resource "aws_route" "public_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id = aws_route_table.public_rt.id
+  destination_cidr_block = local.default_vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "private_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id = aws_route_table.private_rt.id
+  destination_cidr_block = local.default_vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "database_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id = aws_route_table.database_rt.id
+  destination_cidr_block = local.default_vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "default_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id = data.aws_route_table.main.route_table_id
+  destination_cidr_block = var.vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
