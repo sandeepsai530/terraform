f3ca45669e36e5238b7ef4ec907af2784d01a725
sandeepsai530 <sandeepsai530@gmail.com>
2025-06-27 23:40:58 +0530

    added terraform

---
+++ expense-infra-dev/00-vpc/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.84.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "saisandeep-devops-prod"
+    key            = "expense-vpc-dev" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
+    region         = "us-east-1"
+    dynamodb_table = "saisandeep-devops-prod"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
