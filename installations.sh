d49d73a634e1d890885a7671a11411a287c0577d
sandeepsai530 <sandeepsai530@gmail.com>
2025-07-25 16:40:48 +0530

    added terraform

---
+++ installations.sh
@@ -0,0 +1,30 @@
+#docker
+curl https://get.docker.com |bash
+
+#kubernetes
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/kubectl -> this will make everyone access kubectl
+kubectl version
+
+#EKSCTL
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+
+# (Optional) Verify checksum
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+
+sudo install -m 0755 /tmp/eksctl /usr/local/bin 
+
+sudo mv /tmp/eksctl /usr/local/bin
+
+eksctl version 
+
+
