bb438c5822fbd7eeddba48f463eaee7dd1169d5b
sandeepsai530 <sandeepsai530@gmail.com>
2025-08-25 20:13:35 +0530

    added terraform

--- terraform-aws-eks/00-vpc/vpc.tf
+++ terraform-aws-eks/00-vpc/vpc.tf
@@ -9,7 +9,7 @@ module "vpc" {
     public_subnet_cidrs = var.public_subnet_cidrs
     private_subnet_cidrs = var.private_subnet_cidrs
     database_subnet_cidrs = var.database_subnet_cidrs
-    is_peering_required = true
+    is_peering_required = false
 }
 
 # this can be included in module
