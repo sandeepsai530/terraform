b664c41db84e4054d71befaa93cbb6e094714fca
sandeepsai530 <sandeepsai530@gmail.com>
2025-08-26 12:49:14 +0530

    added terraform

--- EKS-AV/backend/main.tf
+++ EKS-AV/backend/main.tf
@@ -1,19 +1,37 @@
+
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "sai-terraform-demo-av"
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "demo-terraform-eks-state-s3-bucket"
 
   lifecycle {
     prevent_destroy = false
   }
 }
 
-resource "aws_dynamodb_table" "av-dynamodb" {
-  name = "terraform-eks-dynamodb"
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-eks-state-locks"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key = "LockID"
+  hash_key     = "LockID"
 
   attribute {
     name = "LockID"
