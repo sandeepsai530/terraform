331f38b44612f28f20d29f1efe47b8ea5cc3554e
sandeepsai530 <sandeepsai530@gmail.com>
2025-10-26 18:27:46 +0530

    updated output file

--- terraform-aws-vpc/outputs.tf
+++ terraform-aws-vpc/outputs.tf
@@ -1,10 +1,10 @@
-/* output "azs_info" {
+output "azs_info" {
   value = data.aws_availability_zones.available.names
 }
 
 output "subnets_info" {
   value = aws_subnet.public_subnet
-} */
+}
 
 output "vpc_id" {
   value = aws_vpc.main.id
