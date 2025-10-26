9e2251cd7fb2c430ee8ad74e00c4b4a9cae181f9
sandeepsai530 <sandeepsai530@gmail.com>
2025-10-26 19:10:30 +0530

    updated output file

--- expense-infra-dev/00-vpc/outputs.tf
+++ expense-infra-dev/00-vpc/outputs.tf
@@ -1,10 +1,10 @@
-/* output "azs_info" {
+/*  output "azs_info" {
   value = module.vpc.azs_info
 }
 
 output "subnets_info" {
   value = module.vpc.subnets_info
-} */
+}  */
 
 output "public_subnet_ids" {
   value = module.vpc.public_subnet_ids
